a1844fbf52073a10c8eb9ea9db063f35bf505e7a
John Beppu <beppu@work.(none)>
2008-05-03 00:35:10 -0700

    aesthetic disappointment

--- lib/Squatting/View.pm
+++ lib/Squatting/View.pm
@@ -22,24 +22,25 @@ sub name : lvalue {
 # $content = $view->render($template)       # render $template
 # $content = $view->render($template, '_')  # render the generic template
 sub _render {
-  my ($self, $template, $alt) = @_;
+  my ($self, $template, $vars, $alt) = @_;
   if (exists $self->{layout} && ($template !~ /^_/)) {
     $template = $alt if defined $alt;
-    join "", $self->{layout}( $self->{$template}() );
+    join "", $self->{layout}($vars, $self->{$template}($vars));
   } else {
     $template = $alt if defined $alt;
-    join "", $self->{$template}();
+    join "", $self->{$template}($vars);
   }
 }
 
 # forward to _render()
 sub AUTOLOAD {
+  my ($self, $vars) = @_;
   my $template = $AUTOLOAD;
   $template =~ s/.*://;
-  if (exists $_[0]->{$template} && ref($_[0]->{$template}) eq 'CODE') {
-    $_[0]->_render($template);
-  } elsif (exists $_[0]->{_}) {
-    $_[0]->_render($template, '_');
+  if (exists $self->{$template} && ref($self->{$template}) eq 'CODE') {
+    $self->_render($template, $vars);
+  } elsif (exists $self->{_}) {
+    $self->_render($template, $vars, '_');
   } else {
     die("$template cannot be rendered.");
   }
