2f8cca64d0733423b45c71d36cbe584194c1f0e8
John Beppu <beppu@work.(none)>
2008-05-13 04:08:37 -0700

    new example, PODServer

---
+++ eg/PODServer.pm
@@ -0,0 +1,80 @@
+package PODServer;
+use base 'Squatting';
+
+package PODServer::Controllers;
+use Squatting ':controllers';
+use File::Find;
+use Data::Dump 'pp';
+use Module::ScanDeps 'add_deps';
+
+our @C = (
+  C(
+    Home => [ '/' ],
+    get  => sub {
+      my ($self) = @_;
+      $self->render('home');
+    }
+  ),
+  C(
+    POD => [ '/pod/(.*)' ],
+    # The job of this controller is to take $module
+    # and find the file that contains the POD for it.
+    get => sub {
+      my ($self, $module) = @_;
+      my $v   = $self->v;
+      my $key = $module;
+      $v->{module} = $module;
+      $key =~ s{\.html$}{};
+      $key =~ s{::}{/}g;
+      $key =~ s/$/.pm/;
+      my $rv = add_deps($key);
+      if ($rv->{$key}) {
+        my $pm_file  = $rv->{$key}->{file};
+        my $pod_file = $pm_file;
+        $pod_file    =~ s/\.pm$/\.pod/;
+        my $target;
+        if (-e $pod_file) {
+          $target = $pod_file;
+        } else {
+          $target = $rv->{$key}->{file};
+        }
+        $v->{pod_file} = $target;
+        $self->render('pod');
+      } else {
+        $self->render('pod_not_found');
+      }
+    }
+  )
+);
+
+package PODServer::Views;
+use Squatting ':views';
+use Pod::Simple;
+use Pod::Simple::HTML;
+$Pod::Simple::HTML::Perldoc_URL_Prefix = '/pod/';
+
+our @V = (
+  V(
+    'html',
+    home => sub {
+      my $url = R('POD', 'Squatting');
+      qq{<a href="$url">Squatting</a>};
+    },
+    pod => sub {
+      my ($self, $v) = @_;
+      my $out;
+      my $pod = Pod::Simple::HTML->new;
+      $pod->index(1);
+      $pod->output_string($out);
+      $pod->parse_file($v->{pod_file});
+      $out =~ s{%3A%3A}{/}g;
+      $out;
+    },
+    pod_not_found => sub {
+      my ($self, $v) = @_;
+      qq{POD for $v->{module} not found.};
+    }
+  )
+);
+
+1;
