b3859c5261d18b76d28e67ff4a9d7be8b11ef19c
John Beppu <beppu@work.(none)>
2008-06-07 23:40:44 -0700

    prepping for the ChatterBox example

---
+++ eg/ChatterBox.pm
@@ -0,0 +1,14 @@
+package ChatterBox;
+use base 'Squatting';
+
+package ChatterBox::Controllers;
+use Squatting ':controllers';
+our @C = (
+);
+
+package ChatterBox::Views;
+use Squatting ':views';
+our @V = (
+);
+
+1;
--- eg/Example.pm
+++ eg/Example.pm
@@ -2,7 +2,12 @@ package Example;
 use base 'Squatting';
 use Example::Controllers;
 use Example::Views;
-use PODServer;
-$Pod::Simple::HTML::Perldoc_URL_Prefix = '/pod/';
-Example->mount('PODServer', '/pod');
+
+#use PODServer;
+#$Pod::Simple::HTML::Perldoc_URL_Prefix = '/pod/';
+#Example->mount('PODServer', '/pod');
+
+#use ChatterBox;
+#Example->mount('ChatterBox', '/chat');
+
 1;
