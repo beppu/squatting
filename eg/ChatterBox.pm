ea1e4c31b6b292ba6ef879314b59204542aafc48
John Beppu <beppu@work.(none)>
2008-06-08 04:20:15 -0700

    fleshing out the chatter box

--- eg/ChatterBox.pm
+++ eg/ChatterBox.pm
@@ -2,13 +2,56 @@ package ChatterBox;
 use base 'Squatting';
 
 package ChatterBox::Controllers;
+use selfvars;
+use base 'Squatting::Q';
 use Squatting ':controllers';
+
 our @C = (
+  C(
+    Home => [ '/' ],
+    get  => sub {
+      $self->render('chatter_box');
+    },
+  ),
+  C(
+    Widget => [ '/@widget' ],
+    get    => sub {
+    },
+  ),
+  C(
+    Event => [ '/@event' ],
+    get   => sub : Q(chatter_box) {
+    },
+    post  => sub {
+    },
+  )
 );
 
 package ChatterBox::Views;
+use selfvars;
 use Squatting ':views';
+use HTML::AsSubs;
+
+# the ~literal pseudo-element -- don't entity escape this content
+sub x {
+  HTML::Element->new('~literal', text => $_[0])
+}
+
+# HTML::AsSubs forgot to implement span.
+sub span {
+  HTML::AsSubs::_elem('span', \@_);
+}
+
 our @V = (
+  V(
+    'html',
+    _css => qq|
+    |,
+    widget => sub {
+    },
+    chatter_box => sub {
+    },
+  )
 );
 
 1;
