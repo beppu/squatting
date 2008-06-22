fd452b787ac5f501a79f5ced073771e9da854fa0
John Beppu <beppu@work.(none)>
2008-06-22 03:59:50 -0700

    working on making squatting framework-independent

--- lib/Squatting/On/Catalyst.pm
+++ lib/Squatting/On/Catalyst.pm
@@ -1,2 +1,33 @@
 package Squatting::On::Catalyst;
+
+use strict;
+use warnings;
+
+# XXX - WORK IN PROGRESS
+
+# In order to embed a Squatting app into an app written in another framework,
+# we need to be able to do the following things.
+#
+# get incoming CGI parameters
+# get incoming HTTP request headers
+# get incoming HTTP method
+# set outgoing HTTP status
+# set outgoing HTTP response headers
+# set outgoing content
+
+# init_cc($controller, $catalyst) -- initialize a controller clone
+sub init_cc {
+  my ($c, $cat) = @_;
+}
+
+sub catalyze {
+  my ($app, $cat) = @_;
+  my ($c, $p)  = &{$app."::D"}($cat->request->path);
+  my $cc       = init_cc($c, $cat);
+  my $content  = $app->service($cc, @$p);
+  $cat->response->status($cc->status);
+  $cat->response->body($content);
+  $cat->response->headers;
+}
+
 1;
--- lib/Squatting/On/Continuity.pm
+++ lib/Squatting/On/Continuity.pm
@@ -1,2 +1,87 @@
 package Squatting::On::Continuity;
+
+use strict;
+use warnings;
+
+# XXX - WORK IN PROGRESS
+
+sub e {
+  my $r = shift;
+  my %env;
+  my $uri = $r->uri;
+  $env{QUERY_STRING}   = $uri->query || '';
+  $env{REQUEST_PATH}   = $uri->path;
+  $env{REQUEST_URI}    = $uri->path_query;
+  $env{REQUEST_METHOD} = $r->method;
+  $r->scan(sub{
+    my ($header, $value) = @_;
+    my $key = uc $header;
+    $key =~ s/-/_/g;
+    $key = "HTTP_$key";
+    $env{$key} = $value;
+  });
+  \%env;
+}
+
+# \%input = i($query_string)  # Extract CGI parameters from QUERY_STRING
+sub i {
+  my $q = CGI->new($_[0]);
+  my %i = $q->Vars;
+  +{ map {
+    if ($i{$_} =~ /\0/) {
+      $_ => [ split("\0", $i{$_}) ];
+    } else {
+      $_ => $i{$_};
+    }
+  } keys %i }
+}
+
+# \%cookies = c($cookie_header)  # Parse Cookie header(s).
+sub c {
+  +{ map { ref($_) ? $_->value : $_ } CGI::Cookie->parse($_[0]) };
+}
+
+# init_cc($controller, $continuity_request) -- initialize a controller clone
+sub init_cc {
+  my ($c, $cr) = @_;
+  my $cc = $c->clone;
+  $cc->cr      = $cr;
+  $cc->env     = e($cr->http_request);
+  $cc->cookies = c($self->env->{HTTP_COOKIE});
+  $cc->input   = i(join('&', grep { defined } ($self->env->{QUERY_STRING}, $cr->request->content)));
+  $cc->headers = { 'Content-Type' => 'text/html' };
+  $cc->v       = {};
+  $cc->status  = 200;
+  $cc;
+}
+
+# App->continue -- Start Continuity's main loop.
+sub continue {
+  my $app = shift;
+
+  # Putting a RESTful face on Continuity since 2008.
+  Continuity->new(
+    port     => 4234,
+    mapper   => Squatting::Mapper->new(
+      app      => $app,
+      callback => sub {
+        my $cr = shift;
+        my ($c, $p)  = &{$app."::D"}($cr->uri->path);
+        my $cc       = init_cc($c, $cr);
+        my $content  = $app->service($cc, @$p);
+        my $response = HTTP::Response->new(
+          $cc->status,
+          HTTP::Status::status_message($cc->status),
+          [%{$cc->{headers}}],
+          $content
+        );
+        $cr->conn->send_response($response);
+        $cr->end_request;
+      },
+      @_
+    ),
+    @_
+  )->loop;
+}
+
 1;
