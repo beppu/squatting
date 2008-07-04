1f55eed8363c9d35ca6010bfd1a669310fbf85a2
John Beppu <beppu@work.(none)>
2008-07-03 20:03:07 -0700

    UniCodePoints

---
+++ eg/UniCodePoints.pm
@@ -0,0 +1,76 @@
+package UniCodePoints;
+use base 'Squatting';
+use strict;
+use warnings;
+
+package UniCodePoints::Controllers;
+use Squatting ':controllers';
+our @C = (
+  C(
+    Home => [ '/' ],
+    get  => sub {
+      my ($self) = @_;
+      my $input = $self->input;
+      my $v     = $self->v;
+      my $start = $input->{start};
+      $start ||= 0;
+      my $count = $input->{count} || 256;
+      $v->{chars} = [ map { chr($_) } ($start .. ($start + $count - 1)) ];
+      $v->{prev} = { start => (($start - $count) < 0) ? 0 : $start - $count };
+      $v->{next} = { start => $start + $count };
+      $self->render('home');
+    }
+  )
+);
+
+package UniCodePoints::Views;
+use Squatting ':views';
+use HTML::AsSubs;
+
+sub x {
+  HTML::Element->new('~literal', text => $_[0])
+}
+
+our @V = (
+  V(
+    'html',
+
+    layout => sub {
+      my ($self, $v, @content) = @_;
+      html(
+        head(
+          title("unicode codepoints"),
+        ),
+        body(
+          x(@content),
+        ),
+      )->as_HTML;
+    },
+
+    home => sub {
+      my ($self, $v) = @_;
+      div(
+        x($self->_pager($v)),
+        table(
+          map { 
+            &tr(
+              td(sprintf('%x', ord($_))),
+              td($_),
+            ) 
+          } @{$v->{chars}}
+        ),
+        x($self->_pager($v)),
+      )->as_HTML;
+    },
+
+    _pager => sub {
+      my ($self, $v) = @_;
+      div(
+        a({ href => R('Home', $v->{prev}) }, "<prev"),
+        x(" | "),
+        a({ href => R('Home', $v->{next}) }, "next>"),
+      )->as_HTML;
+    },
+
+  ),
+);
