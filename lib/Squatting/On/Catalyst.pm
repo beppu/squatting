33b97c4b3c897812874259d3ccd5bbef291b118f
John Beppu <beppu@work.(none)>
2008-07-05 00:31:35 -0700

    i have successfully squatted on catalyst for the first time

--- lib/Squatting/On/Catalyst.pm
+++ lib/Squatting/On/Catalyst.pm
@@ -1,7 +1,9 @@
 package Squatting::On::Catalyst;
 
 use strict;
+no  strict 'refs';
 use warnings;
+use Data::Dump 'pp';
 
 # XXX - WORK IN PROGRESS
 
@@ -10,25 +12,63 @@ use warnings;
 #
 # get incoming CGI parameters
 # get incoming HTTP request headers
+# get incoming `-cookies
 # get incoming HTTP method
 # set outgoing HTTP status
 # set outgoing HTTP response headers
 # set outgoing content
 
+sub e {
+  my $cat = shift;
+  my $req = $cat->req;
+  my $uri = $req->uri;
+  my %env;
+  $env{QUERY_STRING}   = $uri->query || '';
+  $env{REQUEST_PATH}   = $uri->path;
+  $env{REQUEST_URI}    = $uri->path_query;
+  $env{REQUEST_METHOD} = $req->method;
+  my $h = $req->headers;
+  $h->scan(sub{
+    my ($header, $value) = @_;
+    my $key = uc $header;
+    $key =~ s/-/_/g;
+    $key = "HTTP_$key";
+    $env{$key} = $value;
+  });
+  \%env;
+}
+
+sub c {
+  my $cat = shift;
+  # i think this is wrong...  i may have to massage the data some more.
+  $cat->req->cookies;
+}
+
 # init_cc($controller, $catalyst) -- initialize a controller clone
 sub init_cc {
   my ($c, $cat) = @_;
+  my $cc = $c->clone;
+  $cc->env     = e($cat);
+  $cc->cookies = c($cat);
+  $cc->input   = $cat->req->parameters;
+  $cc->headers = { 'Content-Type' => 'text/html' };
+  $cc->v       = {};
+  $cc->status  = 200;
+  $cc;
 }
 
 sub catalyze {
   my ($app, $cat) = @_;
-  my ($c, $p)  = &{$app."::D"}($cat->request->path);
-  my $cc       = init_cc($c, $cat);
-  my $content  = $app->service($cc, @$p);
+  my ($c,   $p)   = &{ $app . "::D" }($cat->request->uri->path);
+  my $cc = init_cc($c, $cat);
+  my $content = $app->service($cc, @$p);
+  my $h       = $cat->response->headers;
+  my $ch      = $cc->headers;
+  for (keys %$ch) {
+    $h->header($_ => $ch->{$_});
+  }
   $cat->response->status($cc->status);
   $cat->response->body($content);
-  $cat->response->headers;
-  $cat;
 }
 
 1;
