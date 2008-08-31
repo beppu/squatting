64da1b6f65874dfa621588fdbb84bb26bddcae69
John Beppu <john.beppu@gmail.com>
2008-08-31 04:06:09 -0700

    parens

--- lib/Squatting.pm
+++ lib/Squatting.pm
@@ -248,8 +248,8 @@ so much that I ported it to Perl.
 
 =item B<Tiny Codebase>
 
-The core of Squatting which includes Squatting, Squatting::Controller, and
-Squatting::View contains about 4.2K of code (after minifying).  Also, the
+The core of Squatting (which includes Squatting, Squatting::Controller, and
+Squatting::View) contains about 4.2K of code (after minifying).  Also, the
 number of Perl module dependencies has been kept down to a minimum.
 
 =item B<RESTful Controllers By Default>
