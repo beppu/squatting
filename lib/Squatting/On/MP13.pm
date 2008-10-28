d56e6895b3a74ec1a17c8768dae90ac4f402d229
John Beppu <john.beppu@gmail.com>
2008-10-28 08:21:14 -0700

    more emacs-vim-harmony

--- lib/Squatting/On/MP13.pm
+++ lib/Squatting/On/MP13.pm
@@ -4,3 +4,14 @@ sub mp13 {
 }
 
 1;
+
+# Local Variables: ***
+# mode: cperl ***
+# indent-tabs-mode: nil ***
+# cperl-close-paren-offset: -2 ***
+# cperl-continued-statement-offset: 2 ***
+# cperl-indent-level: 2 ***
+# cperl-indent-parens-as-block: t ***
+# cperl-tab-always-indent: nil ***
+# End: ***
+# vim:tabstop=8 softtabstop=2 shiftwidth=2 shiftround expandtab
