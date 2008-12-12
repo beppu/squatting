49b6c662f5fb14ff1d5d519022bc3b7b1f8b8433
John Beppu <john.beppu@gmail.com>
2008-12-11 17:11:48 -0800

    more documentation for Squatting::With::Coro::Debug

--- lib/Squatting/With/Coro/Debug.pm
+++ lib/Squatting/With/Coro/Debug.pm
@@ -37,7 +37,53 @@ Connect to Coro::Debug in another terminal
 
   socat readline unix:/tmp/squatting.with.coro.debug
 
+A Coro::Debug session looks like this:
+
+  > ps
+                 PID SC  RSS USES Description              Where
+           142161516 RC 245k   23 [main::]                 [Event.pm:164]
+           142188912 -- 1404    4 [coro manager]           [Coro.pm:358]
+           142189128 N-   84    0 [unblock_sub scheduler]  -
+           142455240 N-   84    0 [Event idle process]     -
+           146549540 -- 7340   14                          [HttpDaemon.pm:426]
+           146549792 -- 2088    5                          [Continuity.pm:436]
+           146552468 UC 3344    6 [Coro::Debug session]    [Coro.pm:257]
+
 =head1 DESCRIPTION
+
+Using this module in conjunction with a Squatting app that's running on top of
+Continuity will provide you with a L<Coro::Debug> server that you can connect
+to using tools like C<socat>.  This will let you inspect the state of your
+Squatting app while its running.
+
+=head1 CONFIG
+
+=over 4
+
+=item with.coro.debug.unix_domain_socket
+
+This should be a string that represents the path of the Unix domain socket
+that Coro::Debug will use.  If this option is not set, the default value
+is F</tmp/squatting.with.coro.debug>.
+
+B<Example>
+
+  $CONFIG{'with.coro.debug.unix_domain_socket'} = '/tmp/coro-debug-socket';
+
+=back
+
+=head1 SEE ALSO
+
+=head2 Perl Modules
+
+L<Coro>,
+L<Coro::Debug>,
+L<Continuity>,
+L<Squatting::On::Continuity>
+
+=head2 socat
+
+L<http://www.dest-unreach.org/socat/>
 
 =cut
 
